f70f1bcd15972bf1910feb9a0b4f734d50554e63
Yoan Guardiola <yguardio@student.42perpignan.fr>
2026-07-09 17:47:31 +0200

    Refactor PacGum constructor to rename start_pos argument to position and change its type to Vec2

--- src/visual/vpacgum.py
+++ src/visual/vpacgum.py
@@ -1,12 +1,11 @@
 from arcade import SpriteCircle, Vec2
 import arcade
-from arcade.types import Point2
 
 
 class PacGum(SpriteCircle):
-    def __init__(self, start_pos: Point2) -> None:
+    def __init__(self, position: Vec2) -> None:
         radius = 5
-        center = start_pos
+        center = position
         super().__init__(
             radius=radius,
             color=arcade.color.WHITE,
