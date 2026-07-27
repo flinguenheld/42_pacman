76be97a3689f45a8218ff7fc8fef0fc25dd7055d
Florent <florent@linguenheld.net>
2026-07-27 17:11:04 +0200

    Entity - Clean VentityMoving

--- src/visual/entities/ventity_moving.py
+++ src/visual/entities/ventity_moving.py
@@ -1,18 +1,13 @@
-from typing import Literal
-
 import arcade
-from arcade import Sprite, SpriteList, Vec2
+from arcade import Vec2
 
 from src.visual.vatlas import VAtlas
 from src.visual.entities.ventity import VEntity
 
 
-type EntityDirection = Literal["wait", "top", "right", "bot", "left"]
-
-
-# ░░░░░░░░░░░░░█░█░█▀▀░█▀█░▀█▀░▀█▀░▀█▀░█░█░░░█▄█░█▀█░█░█░█▀▀░█▄█░█▀▀░█▀█░▀█▀░░
-# ░░░░░░░░░░░░░▀▄▀░█▀▀░█░█░░█░░░█░░░█░░░█░░░░█░█░█░█░▀▄▀░█▀▀░█░█░█▀▀░█░█░░█░░░
-# ░░░░░░░░░░░░░░▀░░▀▀▀░▀░▀░░▀░░▀▀▀░░▀░░░▀░░░░▀░▀░▀▀▀░░▀░░▀▀▀░▀░▀░▀▀▀░▀░▀░░▀░░░
+# ░░░░░░░░░░░░░░░░░░░░░█░█░█▀▀░█▀█░▀█▀░▀█▀░▀█▀░█░█░░░█▄█░█▀█░█░█░▀█▀░█▀█░█▀▀░░
+# ░░░░░░░░░░░░░░░░░░░░░▀▄▀░█▀▀░█░█░░█░░░█░░░█░░░█░░░░█░█░█░█░▀▄▀░░█░░█░█░█░█░░
+# ░░░░░░░░░░░░░░░░░░░░░░▀░░▀▀▀░▀░▀░░▀░░▀▀▀░░▀░░░▀░░░░▀░▀░▀▀▀░░▀░░▀▀▀░▀░▀░▀▀▀░░
 class VEntityMoving(VEntity):
     """
     Add movement options to VEntity
@@ -25,14 +20,19 @@ class VEntityMoving(VEntity):
     """
 
     def __init__(
-        self, atlas: VAtlas, sprite_name: str, position: Vec2
+        self,
+        atlas: VAtlas,
+        sprite_name: str,
+        position: Vec2,
     ) -> None:
         super().__init__(atlas, sprite_name, position)
 
         # Texture helpers --
-        self._current_direction: EntityDirection = "wait"
+        self._current_direction: str = "wait"
 
-    def get_direction_from_vector(self, vector: Vec2) -> EntityDirection:
+    # ########################################################################
+    # ##################################################### GET DIRECTION ####
+    def get_direction(self, vector: Vec2) -> str:
         """
         Takes a non-normalized vector as input and returns the corresponding
         entity direction ("top", "right", "bot", "left").
@@ -42,14 +42,14 @@ class VEntityMoving(VEntity):
 
         # Threshold to avoid detecting small movements.
         # Like an enemy slightly adjusting its position in the maze.
-        minimal_threshold = 0.9
-        if vector.y > minimal_threshold:
+        MINIMAL_THRESHOLD = 0.9
+        if vector.y > MINIMAL_THRESHOLD:
             return "top"
-        elif vector.x > minimal_threshold:
+        elif vector.x > MINIMAL_THRESHOLD:
             return "right"
-        elif vector.y < -minimal_threshold:
+        elif vector.y < -MINIMAL_THRESHOLD:
             return "bot"
-        elif vector.x < -minimal_threshold:
+        elif vector.x < -MINIMAL_THRESHOLD:
             return "left"
         else:
             return "wait"
@@ -58,40 +58,25 @@ class VEntityMoving(VEntity):
     # #################################################### UPDATE TEXTURE ####
     def update_texture(self) -> None:
         """
-        Change the texture of the entity based on its current
-        movement direction.
-        Has to be called at the end of the update method of the entity,
-        after the change_x and change_y values have been updated.
+        Change the texture of the entity based on its current direction.
         """
-        # QUESTION: What do you think of that?
-        # I think this is a better approach
-        requested_direction = self.get_direction_from_vector(
-            Vec2(self.change_x, self.change_y)
-        )
 
-        if self._current_direction != requested_direction:
-            # print(f"update the animation to {requested_direction}")
+        new_dir = self.get_direction(Vec2(self.change_x, self.change_y))
 
-            new_tile = self._atlas.textures[
-                f"{self._sprite_name}_{requested_direction}"
-            ][0]
+        if self._current_direction != new_dir:
+            new_tile = self._atlas.pick_tile(f"{self._sprite_name}_{new_dir}")
 
             if not isinstance(new_tile.texture, arcade.TextureAnimation):
                 raise ValueError("The given texture has to be animated.")
 
             self.animation = new_tile.texture
-            self._current_direction = requested_direction
+            self._current_direction = new_dir
 
-    def get_closest_sprite(self, sprite_list: SpriteList[Sprite]) -> Sprite:
-        closest_sprite_result = arcade.get_closest_sprite(self, sprite_list)
-        _error_msg = (
-            f"Could not find a closest sprite for {self} in {sprite_list}."
-        )
-        assert closest_sprite_result is not None, _error_msg
-
-        (closest_sprite, _) = closest_sprite_result
-        return closest_sprite
-
+    # ########################################################################
+    # ################################################## APPLY DELTA TIME ####
     def apply_delta_time(self, speed: float, delta_time: float) -> float:
+
+        # QUESTION: What is that number ?
+        # QUESTION: Should it be managed somewhere else like Config or VData ?
         multiplier = 500.0
         return speed * multiplier * delta_time
