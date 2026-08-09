1451a9b44dca89268d401d7c0f74952d6ab3a62c
yguardio <242189634+cat66000@users.noreply.github.com>
2026-08-09 19:38:48 +0200

    Visual - Refactor VInstructions and add enemies

--- src/visual/views/vinstructions.py
+++ src/visual/views/vinstructions.py
@@ -51,71 +51,112 @@ class VIinstructions(GWindow):
             SpriteList()
         )
 
-        font_size_factor = 1.4
+        self.header_font_size_factor = 2.0
+        self.font_size_factor = 1.4
 
-        self.gameplay_header = GLabel(
-            atlas=self.atlas,
-            frame=self.frame,
-            text="GAMEPLAY",
-            font_size_factor=2.0,
-            offset_from_center_frame=Vec2(0, 425),
-        )
-        player_tile = self.atlas.pick_tile("player_right")
-        self.player_instructions = GLabel(
-            atlas=self.atlas,
-            frame=self.frame,
-            text="= Player.",
-            font_size_factor=font_size_factor,
-            offset_from_center_frame=Vec2(VData.SPRITE_SIZE * 1.5, 350),
-        )
-        player_sprite = self.atlas.tile_to_sprite(
-            tile=player_tile,
-            center=Vec2(
-                self.player_instructions.left - (VData.SPRITE_SIZE * 1.5),
-                self.player_instructions.center.y,
-            ),
-            sprite_size=int(VData.SPRITE_SIZE * font_size_factor * 1.2),
-        )
-        self._sprite_list.append(player_sprite)
-        # --
-        self.controls_header = GLabel(
-            atlas=self.atlas,
-            frame=self.frame,
-            text="CONTROLS",
-            font_size_factor=2.0,
-            offset_from_center_frame=Vec2(0, 25),
-        )
-        self.controls = GLabel(
-            atlas=self.atlas,
-            frame=self.frame,
-            text=self.controls_text,
-            multiline=True,
-            font_size_factor=font_size_factor,
-            offset_from_center_frame=Vec2(0, -200),
-        )
+        self.setup_gameplay_section()
+        self.setup_entity_sprites()
+        self.setup_controls_section()
 
         self.to_draw_and_update.extend(
             [
                 self._sprite_list,
                 self.menu,
-                self.player_instructions,
-                self.gameplay_header,
-                self.controls_header,
-                self.controls,
             ]
         )
 
-    # ########################################################################
-    # ###################################################### INSTRUCTIONS ####
-    @property
-    def controls_text(self) -> str:
-        controls = (
-            "WASD/ZQSD/Arrow keys = Move player",
-            "ESC = Open pause menu",
-            "Space = Freeze the game",
-            "T = Switch theme (before your first move)",
+    def setup_gameplay_section(self) -> None:
+        self.gameplay_header = GLabel(
+            atlas=self.atlas,
+            frame=self.frame,
+            text="GAMEPLAY",
+            font_size_factor=self.header_font_size_factor,
+            offset_from_center_frame=Vec2(0, 425),
         )
-        return "\n".join(controls)
+        self.player_instructions = GLabel(
+            atlas=self.atlas,
+            frame=self.frame,
+            text="= Player",
+            font_size_factor=self.font_size_factor,
+            offset_from_center_frame=Vec2(VData.SPRITE_SIZE * 1.5, 325),
+        )
+        self.enemy_instructions = GLabel(
+            atlas=self.atlas,
+            frame=self.frame,
+            text="= Enemy",
+            font_size_factor=self.font_size_factor,
+            offset_from_center_frame=Vec2(VData.SPRITE_SIZE * 1.5, 250),
+        )
+        self.to_draw_and_update.extend(
+            [
+                self.gameplay_header,
+                self.player_instructions,
+                self.enemy_instructions,
+            ]
+        )
+
+    def setup_entity_sprites(self) -> None:
+        # Player sprite --
+        self.player_sprite = self.atlas.tile_to_sprite(
+            tile=self.atlas.pick_tile("player_right"),
+            center=Vec2(
+                self.player_instructions.left - (VData.SPRITE_SIZE * 1.5),
+                self.player_instructions.center.y,
+            ),
+            sprite_size=int(VData.SPRITE_SIZE * self.font_size_factor * 1.2),
+        )
+
+        # Enemy sprites --
+        self.enemy_0_sprite = self.atlas.tile_to_sprite(
+            tile=self.atlas.pick_tile("enemy_0_chasing_right"),
+            center=Vec2(
+                self.enemy_instructions.left - (VData.SPRITE_SIZE * 1.5),
+                self.enemy_instructions.center.y,
+            ),
+            sprite_size=int(VData.SPRITE_SIZE * self.font_size_factor * 1.2),
+        )
+        self.enemy_1_sprite = self.atlas.tile_to_sprite(
+            tile=self.atlas.pick_tile("enemy_1_chasing_right"),
+            center=Vec2(
+                self.enemy_instructions.left - (VData.SPRITE_SIZE * 4.0),
+                self.enemy_instructions.center.y,
+            ),
+            sprite_size=int(VData.SPRITE_SIZE * self.font_size_factor * 1.2),
+        )
+
+        # --
+        self._sprite_list.extend(
+            [self.player_sprite, self.enemy_0_sprite, self.enemy_1_sprite]
+        )
+
+    def setup_controls_section(self) -> None:
+        controls_text = "\n".join(
+            (
+                "WASD/ZQSD/Arrow keys = Move player",
+                "ESC = Open pause menu",
+                "Space = Freeze the game",
+                "T = Switch theme (before your first move)",
+            )
+        )
+
+        self.controls_header = GLabel(
+            atlas=self.atlas,
+            frame=self.frame,
+            text="CONTROLS",
+            font_size_factor=self.header_font_size_factor,
+            offset_from_center_frame=Vec2(0, 25),
+        )
+        self.controls_instructions = GLabel(
+            atlas=self.atlas,
+            frame=self.frame,
+            text=controls_text,
+            multiline=True,
+            font_size_factor=self.font_size_factor,
+            offset_from_center_frame=Vec2(0, -175),
+        )
+        self.to_draw_and_update.extend(
+            [self.controls_header, self.controls_instructions]
+        )
 
     # ########################################################################
     # ############################################################## KEYS ####
