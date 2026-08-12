64ed8f6ed3415f606c3eca0e315cf9574757b511
yguardio <242189634+cat66000@users.noreply.github.com>
2026-08-12 23:02:13 +0200

    Use 'uv run' to execute PyInstaller in build_standalone_exe.sh

--- build_standalone_exe.sh
+++ build_standalone_exe.sh
@@ -1,3 +1,3 @@
 # TODO: Add Windows and MacOS support
 rm -rf dist/Pac-Man build/Pac-Man && \
-pyinstaller -F -n Pac-Man --add-data=test_config.json:. --add-data=textures:textures bundle_pac-man.py
+uv run pyinstaller -F -n Pac-Man --add-data=test_config.json:. --add-data=textures:textures bundle_pac-man.py
